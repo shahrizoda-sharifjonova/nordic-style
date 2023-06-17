203eca735976dd75803d107edfa19d44dff6993a
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2023-06-17 21:58:02 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,13 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+const menu = document.querySelector('.menu');
+const headerList = document.querySelector('.header__list');
+const body = document.querySelector('body');
+
+menu.addEventListener('click', (e)=>{
+    menu.classList.toggle('active')
+    headerList.classList.toggle('active')
+    body.classList.toggle('hidden')
+})
