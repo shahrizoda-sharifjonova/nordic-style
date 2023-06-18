67cbd22e84a584b8d6997d0bad502f7e3c7f6650
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2023-06-18 16:00:57 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -15,3 +15,68 @@ menu.addEventListener('click', (e)=>{
     headerList.classList.toggle('active')
     body.classList.toggle('hidden')
 })
+
+gsap.from('.intro__img', {
+    // scrollTrigger: ".intro__img",
+    opacity: 0,
+    x: 200,
+    duration: 1.5,
+    ease: "back",
+    stagger: .2,
+  })
+gsap.from('.about__img', {
+    scrollTrigger: ".about__img",
+    opacity: 0,
+    y: 200,
+    duration: 1.5,
+    ease: "back",
+    stagger: .3,
+ })
+gsap.from('.awards__img', {
+    scrollTrigger: ".awards__img",
+    opacity: 0,
+    y: 200,
+    duration: 1.5,
+    ease: "back",
+    stagger: .2,
+ })
+gsap.from('.design__card', {
+    scrollTrigger: ".design__card",
+    opacity: 0,
+    y: 200,
+    duration: 1.5,
+    ease: "back",
+    stagger: .2,
+ })
+gsap.from('.prices__img', {
+    scrollTrigger: ".prices__img",
+    opacity: 0,
+    x: 200,
+    duration: 1.5,
+    ease: "back",
+    stagger: .2,
+ })
+gsap.from('.contacts__input', {
+    scrollTrigger: ".contacts__input",
+    opacity: 0,
+    x: -200,
+    duration: 1.5,
+    ease: "back",
+    stagger: .2,
+ })
+gsap.from('.contacts__textarea', {
+    scrollTrigger: ".contacts__textarea",
+    opacity: 0,
+    x: -200,
+    duration: 1.5,
+    ease: "back",
+    stagger: .2,
+ })
+gsap.from('.contacts__btn', {
+    scrollTrigger: ".contacts__btn",
+    opacity: 0,
+    x: -200,
+    duration: 1.5,
+    ease: "back",
+    stagger: .2,
+ })
